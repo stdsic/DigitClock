b0fd100e3523a3d47f0e88d994f739ad3ec53b78
Sic <stdsic9900@naver.com>
2026-07-26 14:36:46 +0900

    refactor: improve code style and readability
    - format code according to modern coding standards
    - clean up unnecessary line breaks and whitespaces
    - no functional or logical changes (behavior remains identical)
    
    refactor: 코딩 스타일 개선 및 가독성 향상
    
    - 최신 코딩 스타일에 맞춰 전체적인 포맷 정돈
    - 불필요한 줄바꿈 및 공백 정리
    - 기능 및 로직 변경 없음 (동작 동일)

--- main.cpp
+++ main.cpp
@@ -1,12 +1,16 @@
 #include "resource.h"
 #include <winuser.h>
+
 #define TRAY_NOTIFY     WM_APP + 1
 #define CLASS_NAME      L"DigitClock"
+
 #define max(a,b) (((a) > (b)) ? (a) : (b))
 #define min(a,b) (((a) < (b)) ? (a) : (b))
+
 #define DEFAULT_HEIGHT 16
 #define BORDER 12
 #define EDGE 6
+
 #define GET_X_LPARAM(lParam) ((int)(short)LOWORD(lParam))
 #define GET_Y_LPARAM(lParam) ((int)(short)HIWORD(lParam))
 
@@ -18,7 +22,8 @@ void DrawClock(HWND hWnd, HDC hdc, int FontHeight, HBRUSH hMask);
 void DrawBitmap(HDC hdc, int x, int y, HBITMAP hBitmap);
 LRESULT CALLBACK WndProc(HWND hWnd, UINT iMessage, WPARAM wParam, LPARAM lParam);
 
-int APIENTRY wWinMain(HINSTANCE hInst, HINSTANCE, LPWSTR, int nCmdShow) {
+int APIENTRY wWinMain(HINSTANCE hInst, HINSTANCE, LPWSTR, int nCmdShow) 
+{
     WNDCLASSEX wcex = {
         sizeof(wcex),
         CS_HREDRAW | CS_VREDRAW,
@@ -31,30 +36,33 @@ int APIENTRY wWinMain(HINSTANCE hInst, HINSTANCE, LPWSTR, int nCmdShow) {
         CLASS_NAME,
         NULL
     };
-
     RegisterClassEx(&wcex);
 
-    HWND hWnd = CreateWindowEx(
-            WS_EX_TOOLWINDOW | WS_EX_TOPMOST | WS_EX_NOACTIVATE | WS_EX_LAYERED,
-            CLASS_NAME,
-            CLASS_NAME,
-            WS_POPUP,
-            10, 10, 300, 150,
-            NULL,
-            (HMENU)NULL,
-            hInst,
-            NULL
-            );
+    HWND hWnd = CreateWindowEx(WS_EX_TOOLWINDOW |
+                               WS_EX_TOPMOST |
+                               WS_EX_NOACTIVATE |
+                               WS_EX_LAYERED,
+                               CLASS_NAME,
+                               CLASS_NAME,
+                               WS_POPUP,
+                               10, 10, 300, 150,
+                               NULL,
+                               (HMENU)NULL,
+                               hInst,
+                               NULL);
 
     ShowWindow(hWnd, nCmdShow);
 
     BkIsDark = TRUE;
     MaskColor = RGB(0, 0, 0);
     TextColor = RGB(255, 255, 255);
-	SetLayeredWindowAttributes(hWnd, RGB(0, 0, 0), 255, LWA_ALPHA | LWA_COLORKEY);
+	SetLayeredWindowAttributes(hWnd, 
+            RGB(0, 0, 0),
+            255,
+            LWA_ALPHA | LWA_COLORKEY);
 
     MSG msg;
-    while(GetMessage(&msg, NULL, 0,0)){
+    while (GetMessage(&msg, NULL, 0,0)) {
         TranslateMessage(&msg);
         DispatchMessage(&msg);
     }
@@ -62,18 +70,25 @@ int APIENTRY wWinMain(HINSTANCE hInst, HINSTANCE, LPWSTR, int nCmdShow) {
     return (int)msg.wParam;
 }
 
-LRESULT CALLBACK WndProc(HWND hWnd, UINT iMessage, WPARAM wParam, LPARAM lParam) {
-    static int FontHeight;
+LRESULT CALLBACK WndProc(HWND hWnd, UINT iMessage, WPARAM wParam, LPARAM lParam) 
+{
     static HBITMAP hBitmap, hMaskBitmap;
+
     static BOOL bShow;
+
     static RECT WindowSize;
     static RECT crt, wrt;
+
     static HFONT hFont;
+    static int FontHeight;
+
     HDC hdc, hMemDC, hMaskDC;
-    HBITMAP hOldBitmap, hOldMaskBitmap;
-    HBRUSH hBrush;
-    HFONT hOldFont;
     PAINTSTRUCT ps;
+
+    HBITMAP hOldBitmap, hOldMaskBitmap;
+    HFONT hOldFont;
+    HBRUSH hBrush;
+
     SIZE TimeMaxSize, DateMaxSize;
 
     NOTIFYICONDATA nid;
@@ -82,25 +97,23 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT iMessage, WPARAM wParam, LPARAM lParam)
 
     WCHAR temp[0x20];
 
-    switch(iMessage){
+    switch (iMessage) {
         case WM_CREATE:
             FontHeight = DEFAULT_HEIGHT * 3;
-            hFont = CreateFont(
-                    FontHeight,
-                    0,
-                    0,
-                    0,
-                    FW_NORMAL,
-                    FALSE,
-                    FALSE,
-                    FALSE,
-                    ANSI_CHARSET,
-                    OUT_DEFAULT_PRECIS,
-                    CLIP_DEFAULT_PRECIS,
-                    CLEARTYPE_QUALITY,
-                    FF_MODERN,
-                    L"Consolas"
-                    );
+            hFont = CreateFont(FontHeight,
+                               0,
+                               0,
+                               0,
+                               FW_NORMAL,
+                               FALSE,
+                               FALSE,
+                               FALSE,
+                               ANSI_CHARSET,
+                               OUT_DEFAULT_PRECIS,
+                               CLIP_DEFAULT_PRECIS,
+                               CLEARTYPE_QUALITY,
+                               FF_MODERN,
+                               L"Consolas");
 
             ZeroMemory(&nid, sizeof(nid));
             nid.cbSize = sizeof(NOTIFYICONDATA);
@@ -135,10 +148,10 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT iMessage, WPARAM wParam, LPARAM lParam)
                     hMenu = LoadMenu(GetModuleHandle(NULL), MAKEINTRESOURCE(IDR_MENU1));
                     hPopupMenu = GetSubMenu(hMenu, 0);
                     GetCursorPos(&Mouse);
-                    if(bShow){
+                    if (bShow) {
                         CheckMenuItem(hMenu, IDM_SHOW, MF_BYCOMMAND | MF_CHECKED);
                         CheckMenuItem(hMenu, IDM_HIDE, MF_BYCOMMAND | MF_UNCHECKED);
-                    }else{
+                    } else {
                         CheckMenuItem(hMenu, IDM_SHOW, MF_BYCOMMAND | MF_UNCHECKED);
                         CheckMenuItem(hMenu, IDM_HIDE, MF_BYCOMMAND | MF_CHECKED);
                     }
@@ -154,18 +167,24 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT iMessage, WPARAM wParam, LPARAM lParam)
             return 0;
 
         case WM_COMMAND:
-            switch(wParam){
+            switch (wParam) {
                 case IDM_SHOW:
-                    if(bShow){ break; }
+                    if (bShow)
+                        break;
+
                     bShow = TRUE;
                     SetTimer(hWnd, 1, 100, NULL);
+
                     SetWindowPos(hWnd, NULL, WindowSize.left, WindowSize.top, WindowSize.right - WindowSize.left, WindowSize.bottom - WindowSize.top, SWP_NOZORDER | SWP_NOACTIVATE | SWP_SHOWWINDOW);
                     break;
 
                 case IDM_HIDE:
-                    if(!bShow){ break; }
+                    if (!bShow)
+                        break;
+
                     bShow = FALSE;
                     KillTimer(hWnd, 1);
+
                     SetWindowPos(hWnd, NULL, 0,0,0,0, SWP_NOZORDER | SWP_HIDEWINDOW);
                     break;
 
@@ -173,6 +192,7 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT iMessage, WPARAM wParam, LPARAM lParam)
                     BkIsDark = !BkIsDark;
                     MaskColor = BkIsDark ? RGB(0, 0, 0) : RGB(255, 255, 255);
                     TextColor = BkIsDark ? RGB(255, 255, 255) : RGB(0, 0, 0);
+
                     SetLayeredWindowAttributes(hWnd, MaskColor, 255, LWA_ALPHA | LWA_COLORKEY);
                     SetWindowPos(hWnd, NULL, 0, 0, 0, 0, SWP_NOMOVE | SWP_NOSIZE | SWP_NOZORDER | SWP_FRAMECHANGED);
                     break;
@@ -184,13 +204,13 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT iMessage, WPARAM wParam, LPARAM lParam)
             return 0;
 
         case WM_TIMER:
-            switch(wParam){
+            switch (wParam) {
                 case 1:
                     hdc = GetDC(hWnd);
                     hMemDC = CreateCompatibleDC(hdc);
                     hMaskDC = CreateCompatibleDC(hdc);
 
-                    if(hBitmap == NULL){
+                    if (hBitmap == NULL) {
                         GetClientRect(hWnd, &crt);
                         hBitmap = CreateCompatibleBitmap(hdc, crt.right, crt.bottom);
                         hMaskBitmap = CreateCompatibleBitmap(hdc, crt.right, crt.bottom);
@@ -215,17 +235,18 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT iMessage, WPARAM wParam, LPARAM lParam)
                     ReleaseDC(hWnd, hdc);
                     break;
             }
+
             InvalidateRect(hWnd, NULL, FALSE);
             return 0;
 
         case WM_SIZE:
-            if(SIZE_MINIMIZED != wParam){
-                if(hBitmap){
+            if (SIZE_MINIMIZED != wParam) {
+                if (hBitmap) {
                     DeleteObject(hBitmap);
                     hBitmap = NULL;
                 }
 
-                if(hMaskBitmap){
+                if (hMaskBitmap) {
                     DeleteObject(hMaskBitmap);
                     hMaskBitmap = NULL;
                 }
@@ -239,8 +260,7 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT iMessage, WPARAM wParam, LPARAM lParam)
             EndPaint(hWnd, &ps);
             return 0;
 
-        case WM_WINDOWPOSCHANGING:
-            {
+        case WM_WINDOWPOSCHANGING: {
                 MONITORINFOEX miex;
                 memset(&miex, 0, sizeof(miex));
                 miex.cbSize = sizeof(miex);
@@ -256,6 +276,7 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT iMessage, WPARAM wParam, LPARAM lParam)
                 } else if (abs(lpwp->x + lpwp->cx - miex.rcMonitor.right) < SideSnap) {
                     lpwp->x = miex.rcMonitor.right - lpwp->cx;
                 } 
+
                 if (abs(lpwp->y - miex.rcMonitor.top) < SideSnap) {
                     lpwp->y = miex.rcMonitor.top;
                 } else if (abs(lpwp->y + lpwp->cy - miex.rcMonitor.bottom) < SideSnap) {
@@ -268,20 +289,26 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT iMessage, WPARAM wParam, LPARAM lParam)
             Mouse = { GET_X_LPARAM(lParam), GET_Y_LPARAM(lParam) };
             GetWindowRect(hWnd, &wrt);
 
-            if(Mouse.x >= wrt.left + BORDER && Mouse.x <= wrt.right - BORDER && Mouse.y >= wrt.top + BORDER && Mouse.y <= wrt.bottom - BORDER){
+            if (Mouse.x >= wrt.left + BORDER && 
+                Mouse.x <= wrt.right - BORDER &&
+                Mouse.y >= wrt.top + BORDER &&
+                Mouse.y <= wrt.bottom - BORDER) {
                 return HTCAPTION;
             }
             break;
 
         case WM_DESTROY:
             KillTimer(hWnd, 1);
+
             nid.cbSize = sizeof(NOTIFYICONDATA);
             nid.hWnd = hWnd;
             nid.uID = 0;
             Shell_NotifyIcon(NIM_DELETE, &nid);
+
             DeleteObject(hFont);
             DeleteObject(hMaskBitmap);
             DeleteObject(hBitmap);
+
             PostQuitMessage(0);
             return 0;
     }
@@ -289,7 +316,8 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT iMessage, WPARAM wParam, LPARAM lParam)
     return DefWindowProc(hWnd, iMessage, wParam, lParam);
 }
 
-void DrawClock(HWND hWnd, HDC hdc, int FontHeight, HBRUSH hMask) {
+void DrawClock(HWND hWnd, HDC hdc, int FontHeight, HBRUSH hMask) 
+{
     static const WCHAR* yoil[] = {
         L"일요일",
         L"월요일",
@@ -308,30 +336,32 @@ void DrawClock(HWND hWnd, HDC hdc, int FontHeight, HBRUSH hMask) {
     int hour, min, sec, x, y;
 
     hour = ((st.wHour > 12) ? (st.wHour - 12) : st.wHour);
-    if(st.wHour > 12 && hour == 12) { hour = 0; }
+    if (st.wHour > 12 && hour == 12)
+        hour = 0;
+
     min = st.wMinute;
     sec = st.wSecond;
 
-    wsprintf(
-            Time,
-            L"%02d:%02d:%02d %s %lc\r\n%s, %02d월 %02d일",
-            hour, min, sec,
-            ((st.wHour >= 12) ? L"PM" : L"AM"),
-            0x25A0,
-            yoil[st.wDayOfWeek],
-            st.wMonth,
-            st.wDay
-            );
+    wsprintf(Time,
+             L"%02d:%02d:%02d %s %lc\r\n%s, %02d월 %02d일",
+             hour, min, sec,
+             ((st.wHour >= 12) ? L"PM" : L"AM"),
+             0x25A0,
+             yoil[st.wDayOfWeek],
+             st.wMonth,
+             st.wDay);
 
     RECT crt;
     GetClientRect(hWnd, &crt);
     FillRect(hdc, &crt, hMask);
+
     int BkMode = SetBkMode(hdc, TRANSPARENT);
     COLORREF prev = SetTextColor(hdc, TextColor);
 
     x = 0, y = 0;
     ptr = wcstok(Time, L"\r\n");
-    while(ptr){
+
+    while (ptr) {
         TextOut(hdc, x, y, ptr, wcslen(ptr));
         y += FontHeight;
         ptr = wcstok(NULL, L"\r\n");
@@ -341,7 +371,8 @@ void DrawClock(HWND hWnd, HDC hdc, int FontHeight, HBRUSH hMask) {
     SetBkMode(hdc, BkMode);
 }
 
-void DrawBitmap(HDC hdc, int x, int y, HBITMAP hBitmap){
+void DrawBitmap(HDC hdc, int x, int y, HBITMAP hBitmap)
+{
 	HDC hMemDC = CreateCompatibleDC(hdc);
 	HGDIOBJ hOld = SelectObject(hMemDC, hBitmap);
 
